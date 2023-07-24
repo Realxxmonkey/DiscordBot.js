6b86424b15cff4bbe60d804e816cb2e13e0b547c
Devajuice <deva12jith@gmail.com>
2023-07-24 09:15:10 +0530

    Added a Embed command.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -29,6 +29,20 @@ client.on('interactionCreate', async (interaction) => {
     const num2 = interaction.options.get('second-number').value;
 
     interaction.reply(`The sum is: ${num1 + num2}`);
+  }
+
+  if (interaction.commandName === 'embed') {
+    const embed = new EmbedBuilder()
+      .setTitle('Embed title')
+      .setDescription('This is an embed description.')
+      .setColor('Random')
+      .addFields({
+        name: 'Field title.',
+        value: 'Some random value',
+        inline: true,
+      });
+
+    interaction.reply({ embeds: [embed] });
   }
 });
 
